08726edb91138f68c38ae2592f81850171cdc679
Ivan <114999150+Sychevskyii@users.noreply.github.com>
2024-02-11 15:23:51 +0200

    update task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+      <a class="gallery__link" href="large-image.jpg">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`;
+    })
+    .join("");
+}
+
+galleryEl.addEventListener("click", getLargeImageUrl);
+let largeImageUrl = "";
+
+function getLargeImageUrl(e) {
+  e.preventDefault();
+
+  const isGalleryLink = e.target.classList.contains("gallery__image");
+
+  if (!isGalleryLink) {
+    return;
+  }
+
+  largeImageUrl = e.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${largeImageUrl}" width="800" height="600">
+`);
+
+  instance.show();
+}
